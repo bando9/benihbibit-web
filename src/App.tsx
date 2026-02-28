58d6005a13477750b70ea1f8a4efd5612e3cde03
bando9 <bandomega1@gmail.com>
2026-02-28 13:18:37 +0700

    feat: implement themes shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex items-center flex-col justify-center mt-52">
+        <div className="mb-5 capitalize text-xl font-semibold">home page</div>
+        <div>
+          <Button variant="default">Click</Button>
+          <Button variant="destructive">Click</Button>
+          <Button variant="ghost">Click</Button>
+          <Button variant="link">Click</Button>
+          <Button variant="link">Click</Button>
+          <Button variant="outline">Click</Button>
+          <Button variant="secondary">Click</Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
