bfcc086eb7575750df5aaa63a8b41bd08bcf90c7
bando9 <bandomega1@gmail.com>
2026-04-15 23:42:39 +0700

    feat: install & setup react-cookies

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router/dom"
 import { router } from "./routes/router.tsx"
+import { CookiesProvider } from "react-cookie"
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <CookiesProvider defaultSetOptions={{ path: "/" }}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </CookiesProvider>
   </ThemeProvider>
 )
